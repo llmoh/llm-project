8bc6adb428d7262a3d36cec31509f38da8a707da
Mohamed <mohamed.llaidani@gmail.com>
2026-03-01 14:08:53 +0100

    initial commit for LLM projet

---
+++ src/main/java/com/project/llm/LlmApplication.java
@@ -0,0 +1,13 @@
+package com.project.llm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/llm/LlmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.llm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
